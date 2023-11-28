c8d0bcf594c194a5bec3121fb7abbb1c233ac7a1
stovik <jenda.stovik@gmail.com>
2023-11-28 19:22:57 +0900

    library for proj crated

---
+++ Project_02_Weather_Station/proj2_lib.py
@@ -0,0 +1,35 @@
+import requests
+import serial
+
+def smoothing(x, smoothing_win = 5, overlap = 1):
+    smooth_x = []
+    t = []
+    for i in range(0, len(x), int(smoothing_win*overlap)):
+        smooth_x.append(sum(x[i:i+smoothing_win])/smoothing_win)
+        t.append(i)
+    return t, smooth_x
+
+def login_to_server(ip = "192.168.6.153", user = {"username": "Jenda", "password": "stoikForPres"}):
+    ans = requests.post(f"http://{ip}/login", json=user)
+    #print(ans.json())
+    cookie = ans.json()["access_token"]
+    return {"Authorization": f"Bearer {cookie}"} 
+
+def new_record(value, sensor_id = 12, ip = "192.168.6.153"):
+    headers = login_to_server()
+    record = {"sensor_id": 12, "value": 28}
+    ans = requests.post(f"http://{ip}/reading/new", json=record, headers=headers)
+
+def get_sensor(sensor_id = 12, ip = "192.168.6.153"):
+    headers = login_to_server()
+    sen = requests.get(f"http://{ip}/sensors", headers=headers)
+    return sen.json()
+
+def read_ardruino():
+    ardruino = serial.Serial(port="COM6", baudrate=9600, timeout=0.1)
+    data = ""
+    while len(data) < 1:
+        data = ardruino.readline()
+    return data.decode("utf-8")
+
+    
