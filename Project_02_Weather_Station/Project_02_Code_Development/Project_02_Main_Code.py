3fffd331eefe2c609169e1bda96ccc88a10e0743
Victor Rodas <2025.victor.daniel.rodas.barrios@uwcisak.jp>
2023-11-30 19:10:07 +0900

    Organizaing data

--- Project_02_Weather_Station/Project_02_Code_Development/Project_02_Main_Code.py
+++ Project_02_Weather_Station/Project_02_Code_Development/Project_02_Main_Code.py
@@ -1,6 +1,6 @@
-#collects data from local arduino connected to 3 sensors and saves it to local file and to server
+#collects Project_02_CSV_Files from local arduino connected to 3 sensors and saves it to local file and to server
 
-from proj2_lib import *
+from Project_02_Library import *
 import time
 
 def main():
@@ -12,9 +12,9 @@ def main():
     s3h = create_new_sensor("s3", "Humidity", "room")
     for _ in range(576):
         d1, d2, d3 = read_ardruino()
-        save_localy(data=d1, file_name="data/s1.csv")
-        save_localy(data=d2, file_name="data/s2.csv")
-        save_localy(data=d3, file_name="data/s3.csv")
+        save_localy(data=d1, file_name="../Project_02_CSV_Files/Project_02_CSV_S1.csv")
+        save_localy(data=d2, file_name="../Project_02_CSV_Files/Project_02_CSV_S2.csv")
+        save_localy(data=d3, file_name="../Project_02_CSV_Files/Project_02_CSV_S3.csv")
 
         new_record(d1[1], sensor_id=s1t)
         new_record(d1[0], sensor_id=s1h)
